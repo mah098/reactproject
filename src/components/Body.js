ae9e5d4408e663853a01019ea1f1d31c33b73f84
mah098 <mahendra@greatlearning.in>
2024-12-24 22:55:48 +0530

    using live data from api and showing Loading

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,21 @@
 import ResCard from "./ResCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+//import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 const Body = () => {
-    const [restaurantData, setRestaurantData] = useState(resList)
-    return <>
+    const [restaurantData, setRestaurantData] = useState([])
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const fetchData = async () => {
+        const APIResponse = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const jsonData = await APIResponse.json();
+        //optional chaining
+        setRestaurantData(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    // if (restaurantData.length === 0)
+    //     return <h1>Loading...</h1>
+return restaurantData.length === 0 ? <h1>Loading...</h1> : <>
             <button className= "btn" onClick={ () => {
                 filteredList = restaurantData.filter(
                     (res) => res.info.avgRating > 4.5
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -10,7 +10,7 @@ const ResCard = (props) => {
                      </div>
                      <h1 className="rest-name">{name}</h1>
                      <div className="highlight">
-                             <span>{avgRating}</span><span>{sla.slaString} min</span>
+                             <span>{avgRating}</span><span>{sla.slaString}</span>
                      </div>
                      <p>{cuisines.join(", ")}</p>
                      <p>{areaName}</p>
